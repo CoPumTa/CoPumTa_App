00060a35ee9a92187a500e9b1285a0f528be9355
Hyemin Lee <cotloz88@gmail.com>
2023-07-09 18:50:51 +0900

    신규 회원가입 서버와 연동

--- lib/data/models/register_model.dart
+++ lib/data/models/register_model.dart
@@ -1,16 +1,40 @@
+import 'dart:convert';
+import 'package:client/data/constant.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
 class RegisterModel {
   bool _checked = false;
   String _name = "";
-  String _id = "";
+  String _email = "";
   String _password = "";
 
   bool get checked => _checked;
   String get name => _name;
-  String get id => _id;
+  String get email => _email;
   String get password => _password;
 
   set name(String name) => (_name = name);
-  set id(String id) => (_id = id);
+  set email(String email) => (_email = email);
   set password(String password) => (_password = password);
   set checked(bool value) => (_checked = value);
+
+  void signUp() async {
+    final request = Uri.parse(BASE_URL + "auth/signUp/");
+
+    try {
+      final response = await http.post(request,
+          headers: {"Content-Type": "application/json"},
+          body: json.encode(
+              {"userName": name, "email": email, "password": password}));
+      if (response.statusCode == 401) {
+        debugPrint("회원가입 에러");
+      } else {
+        var data = jsonDecode(response.body);
+        debugPrint(data);
+      }
+    } catch (error) {
+      debugPrint(error.toString());
+    }
+  }
 }
