8b23d3c03d8ccae863facec97f674f62ed7eda44
Hyemin Lee <cotloz88@gmail.com>
2023-07-10 00:30:49 +0900

    resolves rebase conflict

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 import 'data/providers/auth_provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   KakaoSdk.init(nativeAppKey: 'dd7c56431141df80cecd3c764bd7e2ee');
   runApp(ChangeNotifierProvider<AuthProvider>(
       create: (_) => AuthProvider(), // 인증 관련 상태를 제공하는 Auth 클래스를 생성합니다.
